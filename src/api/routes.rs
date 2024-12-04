00d9e93e1ade462f93106c7784caa783f6bcaab4
tmtmtt <13021211703@163.com>
2024-12-04 11:28:36 +0800

    update: 更新文件 routes.rs
    Signed-off-by: tmtmtt <13021211703@163.com>

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -22,7 +22,7 @@ impl<'r> FromRequest<'r> for HmacVerified {
     type Error = ();
 
     async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        // Try both signature headers.
+        // Try both signature headers
         let signature = request.headers().get_one(GITHUB_SIGNATURE_HEADER)
             .or_else(|| request.headers().get_one(GITCODE_SIGNATURE_HEADER));
             
